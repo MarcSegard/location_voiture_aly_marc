f6de4af36ecb7152a390a48d8f0f57cf9f09860b
Yomysai <yamysaisai@icloud.com>
2023-02-14 09:33:19 +0100

    vue imprimer et create vehicule en cours

---
+++ src/vue/ImprimeVhculeShow.java
@@ -0,0 +1,41 @@
+package vue;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ImprimeVhculeShow extends JDialog {
+	private final JPanel contentPanel = new JPanel();
+	/**
+	 * Create the dialog.
+	 */
+	public ImprimeVhculeShow() {
+		setResizable(false);
+		setBounds(500, 90, 700, 500);
+	
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		} 
+	}
+}
