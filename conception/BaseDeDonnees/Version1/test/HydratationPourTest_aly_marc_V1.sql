3204c754378a1034f952804af4219f80392c5c68
Marc <segardmarc@gmail.com>
2023-02-20 12:57:20 +0100

    fix VehiculeDao bug and BdD

--- conception/BaseDeDonnees/Version1/test/HydratationPourTest_aly_marc_V1.sql
+++ conception/BaseDeDonnees/Version1/test/HydratationPourTest_aly_marc_V1.sql
@@ -4,9 +4,12 @@ insert into client (nom_client, prenom_client, email_client, password_client, pe
 values ("SEGARD", "Marc", "segardmarc@gmail.com", password("123"), "6561565267525");
 SELECT distinct LAST_INSERT_ID() into @id_client FROM client;
 
--- insert into agence (nom_agence, adresse_agence, ca_agence, SIRET_agence)
--- values ("ABIS", "2 rue des Fleus Bleues 75004 Paris", 0 , "123 568 941 00056");
--- SELECT distinct LAST_INSERT_ID() into @id_agence FROM agence;
+insert into client (nom_client, prenom_client, email_client, password_client, permis_client)
+values ("SEGARD", "Marc", "segardmarc@agence.fr", password("123"), "6561565267525");
+
+insert into agence (nom_agence, adresse_agence, ca_agence, SIRET_agence)
+values ("ABIS", "2 rue des Fleus Bleues 75004 Paris", 0 , "123 568 941 00056");
+SELECT distinct LAST_INSERT_ID() into @id_agence FROM agence;
 
 
 select id_marque into @id_marque from marque where nom_marque="renault";
@@ -33,17 +36,17 @@ values
 (@id_client, @id_vehicule, @id_facture, now(),now());
 
 -- test procedure stockee
-call addVehicule("abis","berline","super98","renault","Megane","./mon/chemin/monimage.jpg", "rouge", 45.6,
-"TW-000-XX", now(),"super voirure",65000,"GPS");
+-- call addVehicule("abis","berline","super98","renault","Megane","./mon/chemin/monimage.jpg", "rouge", 45.6,
+-- "TW-000-XX", now(),"super voirure",65000,"GPS");
 
-call addVehicule("abis","citadine","diesel","renault","C4", "./mon/chemin/monimage2.jpg", "verte", 60,
-"TW-111-XX", now(),"super voirure",5000,"GPS");
+-- call addVehicule("abis","citadine","diesel","renault","C4", "./mon/chemin/monimage2.jpg", "verte", 60,
+-- "TW-111-XX", now(),"super voirure",5000,"GPS");
 
 -- utilisation de la vue pour voir les infos des véhicules
-select * from liste_vehicule;
+-- select * from liste_vehicule;
 
 -- utilisation de la procedure stockee updateVehicule
-call updateVehicule(6,6,"abis","citadine","diesel","renault","Picasso", "./mon/chemin/update.jpg", "verte", 60,
-"TW-111-XX", now(),"update update",5000,"GPS");
+-- call updateVehicule(6,6,"abis","citadine","diesel","renault","Picasso", "./mon/chemin/update.jpg", "verte", 60,
+-- "TW-111-XX", now(),"update update",5000,"GPS");
 
 
